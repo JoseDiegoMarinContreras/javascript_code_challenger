d93f0aa695186a920473ed797242ebd42ce699af
JoseDiegoMarinContreras <DIEGOmai195@hotmail.com>
2022-07-27 15:55:36 -0600

    Chaging collats_problem to typescript code

--- collatz_problem.ts
+++ collatz_problem.ts
@@ -1,11 +1,11 @@
 'use strict';
 
-function collatz_tracking(val){
+function collatz_tracking(val: number): number[]{
     return collatz_tracking_aux(val, []);
 }
 
-function collatz_tracking_aux(value, tracker){
-    const tracker_updated = [...tracker];
+function collatz_tracking_aux(value: number, tracker: number[]): number[]{
+    const tracker_updated: number[] = [...tracker];
     tracker_updated.push(value);
     if(value === 1){
         return tracker_updated;
