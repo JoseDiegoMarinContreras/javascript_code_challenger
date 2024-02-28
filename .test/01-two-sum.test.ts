596950d1e2a9c503b2bf47a15ccc20bfc09e1486
JoseDiegoMarinContreras <DIEGOmai1995@hotmail.com>
2024-02-28 10:54:03 -0700

    added two sum problem

---
+++ .test/01-two-sum.test.ts
@@ -0,0 +1,18 @@
+import twoSum from '../src/01-two-sum';
+
+describe("Two Sum test", () => {
+    test('Input: nums = [2,7,11,15], target = 9', () => {
+        const [nums, target] = [[2, 7, 11, 15], 9];
+        expect(twoSum(nums, target)).toEqual([0, 1]);
+    });
+
+    test('Input: nums = [3,2,4], target = 6', () => {
+        const [nums, target] = [[3,2,4], 6];
+        expect(twoSum(nums, target)).toEqual([1, 2]);
+    });
+
+    test('Input: nums = [3,3], target = 6', () => {
+        const [nums, target] = [[3,3], 6];
+        expect(twoSum(nums, target)).toEqual([0, 1]);
+    });
+});
---
+++ src/01-two-sum.ts
@@ -0,0 +1,26 @@
+/*
+Description:
+   - Given an array of integers nums and an integer target, return indices of the
+     two numbers such that they add up to target.
+   - You may assume that each input would have exactly one solution, and you may
+     not use the same element twice.
+   - You can return the answer in any order.
+
+Example:
+   - Input: nums = [2,7,11,15], target = 9
+   - Output: [0,1]
+   - Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+*/
+
+// SOLUTIONS
+export default function twoSum(nums: number[], target: number): number[] {
+    const mapper = new Map<number, number>();
+    for(let i = 0; i < nums.length; i++){
+        const num = nums[i];
+        if(mapper.has(target - num)){
+            return [mapper.get(target - num)!, i];
+        }
+        mapper.set(num, i);
+    }
+    return [];
+};
